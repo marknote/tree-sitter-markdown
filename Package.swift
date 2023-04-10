0fad1ac9a5b91dce40b28b90049b8c0ffb92fc6d
MarkNote <coder.sophie@gmail.com>
2023-04-10 19:55:51 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,29 +1,39 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "TreeSitterMarkdown",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
+
     products: [
         .library(name: "TreeSitterMarkdown", targets: ["TreeSitterMarkdown"]),
     ],
-    dependencies: [],
-    targets: [
-        .target(name: "TreeSitterMarkdown",
-                path: ".",
-                exclude: [
-                    "corpus",
-                    "grammar.js",
-                ],
-                sources: [
-                    "src/parser.c",
-                    "src/scanner.cc",
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")]),
 
+    dependencies: [
+    ],
+
+    targets: [
+        
+
+        // Original tree-sitter-markdown exported from https://github.com/ikatyang/tree-sitter-markdown/
+        .target(name: "TreeSitterMarkdown", path: "Sources/tree-sitter-markdown/src",
+                sources: [ "parser.c",
+                           "scanner.cc",
+                           "tree_sitter_markdown/block_context.cc",
+                           "tree_sitter_markdown/block_delimiter.cc",
+                           "tree_sitter_markdown/block_scan.cc",
+                           "tree_sitter_markdown/block_scan_util.generated.cc",
+                           "tree_sitter_markdown/inline_context.cc",
+                           "tree_sitter_markdown/inline_delimiter.cc",
+                           "tree_sitter_markdown/inline_scan.cc",
+                           "tree_sitter_markdown/inline_scan_util.generated.cc",
+                           "tree_sitter_markdown/lexer.cc",
+                           "tree_sitter_markdown/parse_table.cc",
+                           "tree_sitter_markdown/predicate.cc",
+                           "tree_sitter_markdown/predicate_util.generated.cc",
+                           "tree_sitter_markdown/shared_type.cc",
+                           "tree_sitter_markdown/util.cc"
+                         ]),
     ]
 )
